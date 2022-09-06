409ffa9025487e9f946af151746c93a7625b96bb
Yevgeniy Shulimenko <evgeniy.shulimenko@gmail.com>
2022-09-06 10:01:11 +0200

    Домашняя работа за 05.09.2022

--- Homework_20220831/src/Main.java
+++ Homework_20220831/src/Main.java
@@ -13,27 +13,30 @@ public class Main {
         int factor = 1;
         int result = 0;
         // Решение
-        while (factor<=10) {
+        while (factor <= 10) {
             result = num * factor;
             System.out.printf(num + " * " + factor + " = " + result + "%n");
             factor += 1;
         }
 
-            System.out.printf("----------------------------------------------------------------------------------%n");
-            System.out.printf("Second level %n");
-            // Дано целое число num. Необходимо написать программу, которая считает, сколько разрядов в этом числе. Например:
-            //           	3 -> 1
-            //           	343 -> 3
-            //           	98761 -> 5
+        System.out.printf("----------------------------------------------------------------------------------%n");
+        System.out.printf("Second level %n");
+        // Дано целое число num. Необходимо написать программу, которая считает, сколько разрядов в этом числе. Например:
+        //           	3 -> 1
+        //           	343 -> 3
+        //           	98761 -> 5
 
-            long num1 = -5198759;
-            long num2 = num1;
-            int counter = 0;
-            // Решение
-            while (num2!=0) {
-                num2/=10;
-                counter+=1;
-            }
-            System.out.printf(num1 + "  - this number has " + counter + " digits");
+        long num1 = 125444;
+        long num2 = num1;
+        int counter = 1;
+        // Решение
+        while (num2 != 0) {
+            num2 /= 10;
+            counter += 1;
         }
+        System.out.printf(num1 + "  - this number has " + counter + " digits");
     }
+
+
+}
+
---
+++ Homework_20220905/src/Main.java
@@ -0,0 +1,33 @@
+public class Main {
+    public static void main(String[] args) {
+
+        // ƒана строка строка, состо€ща€ из нескольких слов разделенных пробелам, необходимо сформировать и вывести на экран строку вида содержащую первое и последнее слово исходной строки. ѕримечание: слова разделены одним пробелом, строка не содержит знаков препинани€. ќграничение: в данной задаче нельз€ пользоватьс€ стандартными методам String кроме charAt() и length(). Ќапример:
+        //УJava is a very popular languageФ ->  УJava languageФ
+
+        String str = "Java is a very popular language";
+        int index = 0;
+        int index1=0;
+        int index2=0;
+        String str1 = "";
+        String str2 = "";
+        String str3= "";
+        while (index<str.length()&&str.charAt(index)!=' ') {
+            str1+=str.charAt(index);
+            index+=1;
+        }
+        index1=str.length()-1;
+        while (index1>0 && str.charAt(index1)!=' '){
+            str2+=str.charAt(index1);
+            index1-=1;
+        }
+
+        index2=str2.length()-1;
+        while (index2>=0){
+            str3+=str2.charAt(index2);
+            index2-=1;
+        }
+       
+        System.out.println(str1 + str3 );
+
+    }
+}
