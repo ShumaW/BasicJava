394e91d5472355a36c885a2bb840b9a23a26c4fe
Yevgeniy Shulimenko <evgeniy.shulimenko@gmail.com>
2022-09-25 14:17:50 +0200

    ДЗ за 21.09.2022

--- Homework_20220919/src/Main.java
+++ Homework_20220919/src/Main.java
@@ -16,34 +16,41 @@ public class Main {
 
     public static int [] resultArray (int minValue , int maxValue) {
 
-        int [] arr = new int[2];
-        int k = minValue;
-
-
-      for (int index=0 ; index < arr.length && k <= maxValue ; index++) {
-          if (index == arr.length-1) {
-              int [] temp = Arrays.copyOf (arr, arr.length);
-              arr = new int [temp.length * 2];
-              arr = Arrays.copyOf(temp, arr.length);
-              }
-          arr[index] = k;
-          k+=1;
-
-      }
-        System.out.println(Arrays.toString(arr));
-        return arr;
-    }
+//        int [] arr = new int[2];
+//        int k = minValue;
+//
+//
+//      for (int index=0 ; index < arr.length && k <= maxValue ; index++) {
+//          if (index == arr.length-1) {
+//              int [] temp = Arrays.copyOf (arr, arr.length);
+//              arr = new int [temp.length * 2];
+//              arr = Arrays.copyOf(temp, arr.length);
+//              }
+//          arr[index] = k;
+//          k+=1;
+//
+//      }
+//        System.out.println(Arrays.toString(arr));
+//        return arr;
+          int [] result = new int [maxValue - minValue];
+          int index= 0;
+          for (int i = minValue; i < maxValue; i++) {
+            result [index] = i;
+            index+=1;
+         }
+         return result;
+         }
 
 
     // Дан массив int. Реализовать метод, который считает сумму положительных  элементов массива.
     //{3, 2, -1, 5, -3} ->10
-    public static int summArray (int [] arr){
+    public static int summArray (int[] arr){
         int sum = 0;
         for (int index = 0; index < arr.length ; index++) {
             if (arr[index] > 0) {
                 sum += arr[index];
             }
-
+// или вместо if тернарный оператор sum+= (arr[index] > 0 ) ? arr [index] : 0
         }
 
         return sum;
---
+++ Homework_20220921/src/Main.java
@@ -0,0 +1,44 @@
+public class Main {
+    public static void main(String[] args) {
+        int [] arroy = {1,3,6,-1,4,-5,9,-21};
+        String[] words = {"qwe", "axcvbn","qwertyuio'","qas","ijcniejncqpeijncqpi"};
+
+        System.out.println("First level");
+        System.out.println(findMin(arroy));
+        System.out.println("--------------------------------");
+        System.out.println("Second level");
+        System.out.println(findMaxString(words));
+    }
+    // Дан массив целых чисел. Реализовать метод, который возвращает минимальное значение в данном массиве. Например: {1,3,6,-1,4,-5,9} -> -5
+    public static int findMin (int [] arroy) {
+
+        int min = 0;
+        for (int i = 0; i < arroy.length-1; i++) {
+            min = arroy[i];
+            if (min > arroy[i + 1]) {
+                min = arroy[i + 1];
+            }
+
+        }
+
+        return min;
+    }
+
+    // Дан массив строк. Реализовать метод, который возвращает индекс самой длинной строки в данном массиве.      Например: {“qwe”,”axcvbn”,”qwertyuio”,”qas”} -> 2
+    public static int findMaxString (String[] words){
+        int res = 0;
+        for (int i = 0; i < words.length -1; i++) {
+
+           String temp = words[i];
+           String temp1 = words[i+1];
+
+           if (temp.length() < temp1.length()){
+               res = temp1.length();
+           }
+        }
+        return res;
+    }
+
+
+
+}
